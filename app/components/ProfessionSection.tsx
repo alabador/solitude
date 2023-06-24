3ab2b5fea4c460adfaa4209ed001e3f584be5bd4
Austin Labador <austinlabador@gmail.com>
2023-06-24 02:03:24 -0700

    Add icons to professions, edit svg

--- app/components/ProfessionSection.tsx
+++ app/components/ProfessionSection.tsx
@@ -1,13 +1,18 @@
 import ProfessionCard from '@/components/ProfessionCard'
 import React from 'react'
 
+import Mining from '@/public/icons/mining.svg'
+import Chop from '@/public/icons/axe-in-log.svg'
+import Farm from '@/public/icons/pitchfork.svg'
+import Fish from '@/public/icons/fishing-pole.svg'
+
 export default function ProfessionSection() {
   return (
     <section className='p-12 pt-0 md:p-24 md:pt-0 grid grid-cols-2 lg:grid-cols-4 gap-4'>
-        <ProfessionCard title='Mining'/>
-        <ProfessionCard title='Woodcutting'/>
-        <ProfessionCard title='Farming'/>
-        <ProfessionCard title='Fishing'/>
+        <ProfessionCard title='Mining' image={Mining}/>
+        <ProfessionCard title='Woodcutting' image={Chop}/>
+        <ProfessionCard title='Farming'image={Farm}/>
+        <ProfessionCard title='Fishing'image={Fish}/>
     </section>
   )
 }
--- components/ProfessionCard.tsx
+++ components/ProfessionCard.tsx
@@ -1,14 +1,20 @@
-import { StaticImageData } from 'next/image'
+import Image, { StaticImageData } from 'next/image'
 import React from 'react'
 import { albertSans } from '@/styles/fonts'
 
+
 export default function ProfessionCard({title, image}: 
-    {title:string, image?:string|StaticImageData}) {
+    {title:string, image:any}) {
   return (
     <div className="card items-center bg-base-100 shadow-xl">
         <div className={`${albertSans.className} card-body`}>
             <h2 className="card-title">{title}</h2>
         </div>
+        <Image 
+            src={image}
+            alt={`${title} profession icon`}
+            className='p-16'
+        />
     </div>
   )
 }
