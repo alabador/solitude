90a0b05fe43bbe3adb4a3b6137b5e34ad9f7e910
Austin Labador <austinlabador@gmail.com>
2023-06-24 02:33:21 -0700

    Add crafting professions and edit icons

--- app/components/ProfessionSection.tsx
+++ app/components/ProfessionSection.tsx
@@ -1,18 +1,42 @@
 import ProfessionCard from '@/components/ProfessionCard'
 import React from 'react'
+import { albertSans } from '@/styles/fonts'
 
 import Mining from '@/public/icons/mining.svg'
 import Chop from '@/public/icons/axe-in-log.svg'
 import Farm from '@/public/icons/pitchfork.svg'
 import Fish from '@/public/icons/fishing-pole.svg'
 
+import Armouring from '@/public/icons/armor-vest.svg'
+import Tailoring from '@/public/icons/leg-armor.svg'
+import Weaponsmithing from '@/public/icons/gladius.svg'
+import Woodworking from '@/public/icons/spiral-arrow.svg'
+import Jeweling from '@/public/icons/gem-chain.svg'
+import Alchemism from '@/public/icons/potion-ball.svg'
+import Scribing from '@/public/icons/tied-scroll.svg'
+import Cooking from '@/public/icons/marshmallows.svg'
+
 export default function ProfessionSection() {
   return (
-    <section className='p-12 pt-0 md:p-24 md:pt-0 grid grid-cols-2 lg:grid-cols-4 gap-4'>
-        <ProfessionCard title='Mining' image={Mining}/>
-        <ProfessionCard title='Woodcutting' image={Chop}/>
-        <ProfessionCard title='Farming'image={Farm}/>
-        <ProfessionCard title='Fishing'image={Fish}/>
+    <section>
+        <h3 className={`${albertSans.className} text-center pb-12 text-xl font-medium`}>Gathering Professions</h3>
+        <section className='p-12 pt-0 md:p-24 md:pt-0 grid grid-cols-2 lg:grid-cols-4 gap-4'>
+            <ProfessionCard title='Mining' image={Mining}/>
+            <ProfessionCard title='Woodcutting' image={Chop}/>
+            <ProfessionCard title='Farming'image={Farm}/>
+            <ProfessionCard title='Fishing'image={Fish}/>
+        </section>
+        <h3 className={`${albertSans.className} text-center pb-12 text-xl font-medium`}>Crafting Professions</h3>
+        <section className='p-12 pt-0 md:p-24 md:pt-0 grid grid-cols-2 lg:grid-cols-4 gap-4'>
+            <ProfessionCard title='Armouring' image={Armouring}/>
+            <ProfessionCard title='Tailoring' image={Tailoring}/>
+            <ProfessionCard title='Weaponsmithing' image={Weaponsmithing}/>
+            <ProfessionCard title='Woodworking' image={Woodworking}/>
+            <ProfessionCard title='Jeweling' image={Jeweling}/>
+            <ProfessionCard title='Alchemism' image={Alchemism}/>
+            <ProfessionCard title='Scribing' image={Scribing}/>
+            <ProfessionCard title='Cooking' image={Cooking}/>
+        </section>
     </section>
   )
 }
