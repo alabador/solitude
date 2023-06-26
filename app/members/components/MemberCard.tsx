a90bafab5bb8fdd3a81552c584cc88e981aa9bfd
Austin Labador <austinlabador@gmail.com>
2023-06-26 02:51:46 -0700

    Add online status badge to member card

--- app/members/components/MemberCard.tsx
+++ app/members/components/MemberCard.tsx
@@ -1,24 +1,42 @@
 import Image from "next/image";
 
-export default function MemberCard({ name, rank, uuid }: { name:string, rank:string, uuid:string }) {
-//   const uuid = userData.uuid;
-  const avatarImageUrl = `https://visage.surgeplay.com/full/${uuid}`;
+async function getUserData(user:string) {
+    const userDataUrl = `https://api.wynncraft.com/v2/player/${user}/stats`
+    const res = await fetch(userDataUrl);
+  
+    if(!res.ok) {
+      throw new Error('Failed to fetch data');
+    }
+  
+    return res.json();
+  }
 
-  return (
-    <div className="card lg:card-side bg-base-100 shadow-xl">
-      <figure className="p-4">
-        <Image 
-            src={avatarImageUrl} 
-            alt="Album"
-            width={150} 
-            height={250}
-        />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">{name}</h2>
-        <h3>{rank}</h3>
-        <p>Click the button to listen on Spotiwhy app.</p>
-      </div>
-    </div>
-  );
+export default async function MemberCard({ name, rank, uuid }: { name:string, rank:string, uuid:string }) {
+
+    const avatarImageUrl = `https://visage.surgeplay.com/full/${uuid}`;
+    const userData = await getUserData(name);
+    let isOnline = userData.data[0].meta.location.online;
+
+    return (
+        <div className="card lg:card-side bg-base-100 shadow-xl relative">
+            <span 
+                className={`badge absolute text-white top-4 right-4 ${isOnline ? 'bg-green-500' : 'bg-red-600'}`}
+            >
+                {isOnline ? "Online" : "Offline"}
+            </span>
+            <figure className="p-4">
+                <Image 
+                    src={avatarImageUrl} 
+                    alt="Album"
+                    width={150} 
+                    height={250}
+                />
+            </figure>
+            <div className="card-body">
+                <h2 className="card-title">{name}</h2>
+                <h3>{rank}</h3>
+                <p>Click the button to listen on Spotiwhy app.</p>
+            </div>
+        </div>
+    );
 }
--- app/members/components/MemberSection.tsx
+++ app/members/components/MemberSection.tsx
@@ -1,15 +1,5 @@
 import MemberCard from "./MemberCard";
 
-async function getUserData(user:string) {
-    const userDataUrl = `https://api.wynncraft.com/v2/player/${user}/stats`
-    const res = await fetch(userDataUrl);
-  
-    if(!res.ok) {
-      throw new Error('Failed to fetch data');
-    }
-  
-    return res.json();
-  }
 
 export default async function MemberSection({data}:
     {data:Promise<{members:[]}>}) {
