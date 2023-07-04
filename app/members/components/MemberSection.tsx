97190241b8577d781ce16bf7022a5f3dcb5802fb
Austin Labador <austinlabador@gmail.com>
2023-07-03 23:51:03 -0700

    Add high ranks and standard ranks

--- app/members/components/MemberSection.tsx
+++ app/members/components/MemberSection.tsx
@@ -1,9 +1,24 @@
 import MemberCard from "./MemberCard";
 
+interface memberProps {
+    name: string,
+    uuid: string,
+    rank: string,
+    contributed: number,
+    joined: any,
+    joinedFriendly: any
+}
 
 export default async function MemberSection({data}:
     {data:Promise<{members:[]}>}) {
     const members = (await data).members;
+    const highRanks = members.filter((member:memberProps) => {
+        return member.rank == "OWNER" || member.rank == "CHIEF"
+    });
+    const standardMembers = members.filter((member:memberProps) => {
+        return member.rank !== "OWNER" && member.rank !== "CHIEF"
+    });
+
 
     return (
         <section className="p-8 grid grid-cols-1 lg:grid-cols-2 gap-8">
